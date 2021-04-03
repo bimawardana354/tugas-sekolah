a0027911a802d2159c7f7838ff6df379dedc5b18
bimawardana354 <68363385+bimawardana354@users.noreply.github.com>
2021-04-03 11:52:05 +0700

    delete tugas portofolio web dinamis

--- tugas portofoilo web dinamis/footer.php
+++
@@ -1,7 +0,0 @@
-    <!-- Footer -->
-
-    <footer class="text-white text-center pb-4">
-      <p>Created with <i class="bi bi-heart-fill text-danger"></i> by <a href="" class="text-white fw-bold">Bima Wardana</a></p>
-    </footer>
-
-    <!-- Akhir Footer -->
